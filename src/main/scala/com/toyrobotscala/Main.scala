10310e85e0362cccc3013e1fa6c55d3ae7b6640d
Bharg Sharma <bharg.sharma@rea-group.com>
2020-02-27 14:44:01 +1100

    Add IO for side effects

--- src/main/scala/com/toyrobotscala/Main.scala
+++ src/main/scala/com/toyrobotscala/Main.scala
@@ -1,4 +1,5 @@
 package com.toyrobotscala
+import cats.effect.IO
 import com.toyrobotscala.Direction.North
 
 import scala.io.StdIn.readLine
@@ -7,7 +8,7 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    println("👋🏽 From Toy 🤖 App")
+    printLine(("👋🏽 From Toy 🤖 App")).unsafeRunSync()
 
     val robot = Robot(0, 0, North)
     val grid = Grid(Range(0, 5), Range(0, 5))
@@ -15,19 +16,23 @@ object Main {
 
     go(initialGame)
   }
-  @scala.annotation.tailrec
-  def go(game: Game): Nothing = {
-    val str = readLine().toUpperCase
-    val maybeComand = Input.parseValidCommand.lift(str)
-
-    maybeComand match {
-      case Some(Command.Report) =>
-        println(game.currentRobot)
-        go(game)
-      case Some(command) =>
-        val newGame = game.play(command)
-        go(newGame)
-      case None => go(game)
-    }
+  def go(game: Game): IO[Unit] = runIoOfMaybeCommand match {
+    case Some(Command.Report) =>
+      runIoOfReportCommand(game)
+      go(game)
+    case Some(command) =>
+      val newGame = game.play(command)
+      go(newGame)
+    case None => go(game)
+  }
+  private def printLine(str: String): IO[Unit] = IO(println(str))
+  private def getInput: IO[String] = IO(readLine().toUpperCase)
+  private def runIoOfMaybeCommand: Option[Command] = {
+    getInput
+      .map(inputStr => Input.parseValidCommand.lift(inputStr))
+      .unsafeRunSync()
+  }
+  private def runIoOfReportCommand(game: Game): Unit = {
+    printLine(game.currentRobot.toString).unsafeRunSync()
   }
 }

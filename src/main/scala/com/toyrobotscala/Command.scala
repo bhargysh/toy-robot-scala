81dfe34a48c9db3b8efd3796a6377e085183c321
Bharg Sharma <bharg.sharma@rea-group.com>
2020-02-27 16:56:41 +1100

    Refactor so commands don't execute what isn't needed

--- src/main/scala/com/toyrobotscala/Command.scala
+++ src/main/scala/com/toyrobotscala/Command.scala
@@ -2,67 +2,73 @@ package com.toyrobotscala
 
 import com.toyrobotscala.Direction.{East, North, South, West}
 
-sealed trait Command {
-  def execute(grid: Grid, robot: Robot): Robot
-}
+sealed trait Command
 
 object Command {
-  case class Place(x: Int, y:Int, direction: Direction) extends Command {
+  sealed trait UpdateRobotPosition extends Command {
+    def execute(grid: Grid, robot: Robot): Robot
+  }
+  sealed trait RotateRobot extends Command {
+    def rotate(robot: Robot): Robot
+  }
+  sealed trait UnchangedRobot extends Command
+
+  case class Place(x: Int, y: Int, direction: Direction)
+      extends UpdateRobotPosition {
     override def execute(grid: Grid, robot: Robot): Robot = {
-      if(robot.hasValidPositionOn(grid) && grid.x.contains(x) && grid.y.contains(y)) {
+      if (robot.hasValidPositionOn(grid) && grid.x.contains(x) && grid.y
+            .contains(y)) {
         Robot(x, y, direction)
       } else {
         robot
       }
     }
   }
-  case object Left extends Command {
-    override def execute(grid: Grid, robot: Robot): Robot = {
-      if(robot.hasValidPositionOn(grid)) {
-        robot.direction match {
-          case North => robot.copy(direction = West)
-          case East => robot.copy(direction = North)
-          case South => robot.copy(direction = East)
-          case West => robot.copy(direction = South)
-        }
-      } else {
-        robot
-      }
-    }
-  }
-  case object Right extends Command { //TODO: left and right just move in 90deg angle, we don't care about a current robot or the grid
-    override def execute(grid: Grid, robot: Robot): Robot = {
-      if(robot.hasValidPositionOn(grid)) {
-        robot.direction match {
-          case North => robot.copy(direction = East)
-          case East => robot.copy(direction = South)
-          case South => robot.copy(direction = West)
-          case West => robot.copy(direction = North)
-        }
-      } else {
-        robot
-      }
-    }
-  }
-  case object Move extends Command {
+
+  case object Move extends UpdateRobotPosition {
     override def execute(grid: Grid, robot: Robot): Robot = {
       if (robot.hasValidPositionOn(grid)) {
         robot.direction match {
-          case North => newRobotOrCurrent(grid, robot.copy(y = robot.y + 1), robot)
-          case East => newRobotOrCurrent(grid, robot.copy(x = robot.x + 1), robot)
-          case South => newRobotOrCurrent(grid, robot.copy(y = robot.y - 1), robot)
-          case West => newRobotOrCurrent(grid, robot.copy(x = robot.x - 1), robot)
+          case North =>
+            newRobotOrCurrent(grid, robot.copy(y = robot.y + 1), robot)
+          case East =>
+            newRobotOrCurrent(grid, robot.copy(x = robot.x + 1), robot)
+          case South =>
+            newRobotOrCurrent(grid, robot.copy(y = robot.y - 1), robot)
+          case West =>
+            newRobotOrCurrent(grid, robot.copy(x = robot.x - 1), robot)
         }
       } else {
         robot
       }
     }
   }
-  case object Report extends Command {
-    override def execute(grid: Grid, robot: Robot): Robot = robot
+
+  case object Right extends RotateRobot {
+    override def rotate(robot: Robot): Robot =
+      robot.direction match {
+        case North => robot.copy(direction = East)
+        case East  => robot.copy(direction = South)
+        case South => robot.copy(direction = West)
+        case West  => robot.copy(direction = North)
+      }
   }
-  private def newRobotOrCurrent(grid: Grid, newRobot: Robot, currentRobot: Robot): Robot = {
-    if(newRobot.hasValidPositionOn(grid)) newRobot else currentRobot
-    //  TODO: ignore the command if it can't move (e.g. about to fall)
+
+  case object Left extends RotateRobot {
+    override def rotate(robot: Robot): Robot =
+      robot.direction match {
+        case North => robot.copy(direction = West)
+        case East  => robot.copy(direction = North)
+        case South => robot.copy(direction = East)
+        case West  => robot.copy(direction = South)
+      }
+  }
+
+  case object Report extends UnchangedRobot
+
+  private def newRobotOrCurrent(grid: Grid,
+                                newRobot: Robot,
+                                currentRobot: Robot): Robot = {
+    if (newRobot.hasValidPositionOn(grid)) newRobot else currentRobot
   }
 }
--- src/main/scala/com/toyrobotscala/Game.scala
+++ src/main/scala/com/toyrobotscala/Game.scala
@@ -3,6 +3,10 @@ package com.toyrobotscala
 case class Game(grid: Grid, currentRobot: Robot) {
 
   def play(command: Command): Game = {
-    Game(grid, command.execute(grid, currentRobot))
+    command match {
+      case position: Command.UpdateRobotPosition => Game(grid, position.execute(grid, currentRobot))
+      case rotateRobot: Command.RotateRobot => Game(grid, rotateRobot.rotate(currentRobot))
+      case _: Command.UnchangedRobot => Game(grid, currentRobot)
+    }
   }
 }
